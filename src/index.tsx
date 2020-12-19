6728098ea4cc4fb7cb0c36ea54b48f35ebfc7b15
Nick Barnette <nicdavid@live.unc.edu>
2020-12-19 13:58:43 -0500

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -2,54 +2,55 @@ import React, { lazy, FC } from 'react';
 import ReactDOM from 'react-dom';
 
 // App state
-// import { Provider } from 'react-redux';
-// import storeConfig from './store/configureStore';
-// import { PersistGate } from 'redux-persist/integration/react';
-// const store = storeConfig();
+import { Provider } from 'react-redux';
+import storeConfig from './store/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
+const store = storeConfig();
 
 // Views
-// const HomeView = lazy(() => import('./views/HomeView'));
+const HomeView = lazy(() => import('./views/HomeView'));
 
 // Components
-// import {
-// 	AppRoot,
-// 	NavigationBar,
-// 	BusyIndicator,
-// 	NavigationLink,
-// 	DefaultLightTheme,
-// 	DefaultDarkTheme,
-// } from '@nickbarnette/dashui';
+import {
+	AppRoot,
+	NavigationBar,
+	BusyIndicator,
+	NavigationLink,
+	DefaultLightTheme,
+	DefaultDarkTheme,
+} from '@nickbarnette/dashui';
 
 // Styles
 import './app.scss';
 
 const App: FC = () => {
+	console.log(store.store.getState());
 	return (
-		// <Provider store={store.store}>
-		// 	<PersistGate
-		// 		loading={<BusyIndicator />}
-		// 		persistor={store.persistor}
-		// 	>
-		// 		<AppRoot
-		// 			customLightTheme={DefaultLightTheme}
-		// 			customDarkTheme={DefaultDarkTheme}
-		// 			topNavigation={
-		// 				<NavigationBar
-		// 					center={
-		// 						<NavigationLink link='/'>
-		// 							Dash Design
-		// 						</NavigationLink>
-		// 					}
-		// 				/>
-		// 			}
-		// 		>
-		// 			<HomeView />
-		// 		</AppRoot>
-		// 	</PersistGate>
-		// </Provider>
-		<div>
-			<span>testing</span>
-		</div>
+		<Provider store={store.store}>
+			<PersistGate
+				loading={<BusyIndicator />}
+				persistor={store.persistor}
+			>
+				<div>
+					<span>testing</span>
+				</div>
+				{/* <AppRoot
+					customLightTheme={DefaultLightTheme}
+					customDarkTheme={DefaultDarkTheme}
+					topNavigation={
+						<NavigationBar
+							center={
+								<NavigationLink link="/">
+									Dash Design
+								</NavigationLink>
+							}
+						/>
+					}
+				>
+					<HomeView />
+				</AppRoot> */}
+			</PersistGate>
+		</Provider>
 	);
 };
 
